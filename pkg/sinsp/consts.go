233e1ea29731645ce2c65ba25f1da98ffe01e75f
Leonardo Grasso <me@leonardograsso.com>
2021-02-07 14:47:23 +0100

    update(pkg/sinsp): more framework conts
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- pkg/sinsp/consts.go
+++ pkg/sinsp/consts.go
@@ -2,9 +2,16 @@ package sinsp
 
 // SCAP return types
 const (
-	ScapSuccess int32 = 0
-	ScapFailure int32 = 1
-	ScapTimeout int32 = -1
+	ScapSuccess         int32 = 0
+	ScapFailure         int32 = 1
+	ScapTimeout         int32 = -1
+	ScapIllegalInput    int32 = 3
+	ScapNotFound        int32 = 4
+	ScapInputTooSmall   int32 = 5
+	ScapEOF             int32 = 6
+	ScapUnexpectedBlock int32 = 7
+	ScapVersionMismatch int32 = 8
+	ScapNotSupported    int32 = 9
 )
 
 // Plugin types
