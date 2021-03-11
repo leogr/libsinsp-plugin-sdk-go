a852161438b5e6eb8e9fd52c0401666c6691507a
Loris Degioanni <loris@sysdig.com>
2021-03-11 15:51:18 -0800

    fields now have IDs

--- pkg/sinsp/fields.go
+++ pkg/sinsp/fields.go
@@ -4,6 +4,7 @@ package sinsp
 // Should be used when implementing plugin_get_fields().
 type FieldEntry struct {
 	Type string `json:"type"`
+	ID   uint32 `json: "ID"`
 	Name string `json:"name"`
 	Desc string `json:"desc"`
 }
