6de33290116f3dca6581d155329d21e991e15cab
Loris Degioanni <loris@sysdig.com>
2021-04-14 18:03:48 -0700

    new field property: hidden

--- pkg/sinsp/fields.go
+++ pkg/sinsp/fields.go
@@ -3,9 +3,10 @@ package sinsp
 // FieldEntry represents a single field entry that an extractor plugin can expose.
 // Should be used when implementing plugin_get_fields().
 type FieldEntry struct {
-	Type string `json:"type"`
-	ID   uint32 `json: "ID"`
-	Name string `json:"name"`
+	Type    string `json:"type"`
+	ID      uint32 `json: "ID"`
+	Name    string `json:"name"`
 	Display string `json:"display"`
-	Desc string `json:"desc"`
+	Desc    string `json:"desc"`
+	Hidden  bool   `json:"hidden"`
 }
