70a04339f2a39389c8d0228556ec240c31c51f20
Loris Degioanni <loris@sysdig.com>
2021-04-27 17:22:43 -0700

    updates in the fields properties

--- pkg/sinsp/fields.go
+++ pkg/sinsp/fields.go
@@ -3,10 +3,10 @@ package sinsp
 // FieldEntry represents a single field entry that an extractor plugin can expose.
 // Should be used when implementing plugin_get_fields().
 type FieldEntry struct {
-	Type    string `json:"type"`
-	ID      uint32 `json: "ID"`
-	Name    string `json:"name"`
-	Display string `json:"display"`
-	Desc    string `json:"desc"`
-	Visibility  string   `json:"visibility"`
+	Type       string `json:"type"`
+	ID         uint32 `json: "ID"`
+	Name       string `json:"name"`
+	Display    string `json:"display"`
+	Desc       string `json:"desc"`
+	Properties string `json:"properties"`
 }
